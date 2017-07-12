5fe8614c234deee5a8e748909a2859f850901f3c
Ronak H Shah <rons.shah@gmail.com>
2017-07-12 19:18:44 -0400

    Debugging

--- iAnnotateSV/AnnotateEachBreakpoint.py
+++ iAnnotateSV/AnnotateEachBreakpoint.py
@@ -48,7 +48,7 @@ def AnnotateEachBreakpoint(chromosome,position,strand,df,autoSelect):
                 d = df.iloc[tindex]['txStart'] - position
                 apList = [c,d,e,d1,d2,e1,e2,f]
                 coordData.loc[tindex,['c', 'd', 'e', 'd1', 'd2', 'e1', 'e2','f']] = apList
-                #print "In Promoter","c=",c,"d=",d,"e=",e,"e1=",e1,"e2=",e2,"d1=",d1,"d2=",d2,"f=",f
+                print "In Promoter 1","c=",c,"d=",d,"e=",e,"e1=",e1,"e2=",e2,"d1=",d1,"d2=",d2,"f=",f
                 continue
             else:
                 c = None
@@ -59,7 +59,7 @@ def AnnotateEachBreakpoint(chromosome,position,strand,df,autoSelect):
                 apList = [c,d,e,d1,d2,e1,e2,f]
                 #coordData.add(apList)
                 coordData.loc[tindex,['c', 'd', 'e', 'd1', 'd2', 'e1', 'e2','f']] = apList
-                #print "In Promoter","c=",c,"d=",d,"e=",e,"e1=",e1,"e2=",e2,"d1=",d1,"d2=",d2,"f=",f
+                print "In Promoter 2","c=",c,"d=",d,"e=",e,"e1=",e1,"e2=",e2,"d1=",d1,"d2=",d2,"f=",f
                 continue
             else:
                 c = None
